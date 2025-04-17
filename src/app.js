296809f095e7d4bd94fd0d3944b09ce55c93c40b
noinvis <lazizbek.sn@gmail.com>
2025-04-17 05:08:44 +0500

    First

---
+++ src/app.js
@@ -0,0 +1,50 @@
+{
+    // 1 - task
+    // let a = prompt("a sonini kiriting >>>");
+    // let yaxlit = Math.round(a);
+    // console.log("Yaxlitlangan son:", yaxlit);
+}
+
+{
+    // 2 - task
+    // let a = prompt("a sonini kiriting >>>");
+    // let modul = Math.abs(a);
+    // console.log("Modul:", modul);
+}
+
+{
+    // 3 - task
+    // let baholar = [];
+    // let summa = 0;
+    // for (let i = 0; i < 5; i++) {
+    //     let ball = Math.floor(Math.random() * 101);
+    //     baholar.push(ball);
+    //     summa += ball;
+    // }
+    // let ortacha = summa / baholar.length;
+    // console.log("Baholar:", baholar);
+    // console.log("O'rtacha ball:", Math.round(ortacha));
+}
+
+{
+    // 4 - task
+    // let kurs = (Math.random() * (12.5 - 10.5) + 10.5);
+    // let dollar = 100;
+    // let som = dollar * kurs;
+    // console.log("Kurs:", kurs.toFixed(4));
+    // console.log("So'mda:", som.toFixed(2));
+}
+
+{
+    // 5 - task
+    // let yil = Math.floor(Math.random() * 26) + 2000;
+    // let oy = Math.floor(Math.random() * 12) + 1;
+    // let kun = Math.floor(Math.random() * 28) + 1;
+    // if (oy < 10) {
+    //     oy = "0" + oy;
+    // }
+    // if (kun < 10) {
+    //     kun = "0" + kun;
+    // }
+    // console.log(`Tasodifiy sana: ${yil}-${oy}-${kun}`);
+}
